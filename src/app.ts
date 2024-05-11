4daacd1ad46eeb5185dfb9c55fc48f57b5457461
rashedrk <rashedrk001@gmail.com>
2024-05-11 22:35:31 +0600

    final fix

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import router from "./app/routes";
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://eye-glass-management-v2-client.vercel.app'], credentials: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Eye glass server v2.0');
